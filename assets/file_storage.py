fc7aa8fe49eed2aec54c8cd3031308007530cb47
saqib <saqibullah@msn.com>
2020-04-06 18:46:39 +0500

    replace urllib from urllib.request

--- assets/file_storage.py
+++ assets/file_storage.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import cloudinary
 from cloudinary.uploader import upload
 from config import asset
 from utilities import constant
 import numpy as np
-import urllib
+import urllib.request
 import cv2
 
+
 class file_manager:
+
     '''Using this class user can upload different asset in different repositories'''
-    
-    def __init__(self,storage_type):
+
+    def __init__(self, storage_type):
         if storage_type is None:
             self.storage_type = constant.STORAGE_TYPE_CLOUDINARY
         else:
@@ -21,34 +26,42 @@ class file_manager:
             cloud_name = config.cloud_name
             api_key = config.api_key
             api_secret = config.api_secret
-            cloudinary.config(cloud_name = cloud_name, api_key = api_key, api_secret = api_secret)
+            cloudinary.config(cloud_name=cloud_name, api_key=api_key,
+                              api_secret=api_secret)
             return
         elif self.storage_type == constant.STORAGE_TYPE_S3:
             pass
-    
-    def upload_file(self,file_name, custom_name=None):
-        if  self.storage_type == constant.STORAGE_TYPE_CLOUDINARY:
+
+    def upload_file(self, file_name, custom_name=None):
+        if self.storage_type == constant.STORAGE_TYPE_CLOUDINARY:
             if custom_name is None:
-                return upload(file_name)            
+                return upload(file_name)
             else:
-                return upload(file_name, public_id = custom_name)
+                return upload(file_name, public_id=custom_name)
         elif self.storage_type == constant.STORAGE_TYPE_S3:
             pass
-        
-    def get_file_url(self,file_name):
-        if  self.storage_type == constant.STORAGE_TYPE_CLOUDINARY:
+
+    def get_file_url(self, file_name):
+        if self.storage_type == constant.STORAGE_TYPE_CLOUDINARY:
             return cloudinary.utils.cloudinary_url(file_name)
         elif self.storage_type == constant.STORAGE_TYPE_S3:
             pass
-    
-    def get_image(self,file_name):
-        if  self.storage_type == constant.STORAGE_TYPE_CLOUDINARY:
-            # download the image, convert it to a NumPy array, and then read
-	        # it into OpenCV format
-	        resp = urllib.urlopen(file_name)
-	        image = np.asarray(bytearray(resp.read()), dtype="uint8")
-	        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    def get_image(self, file_name):
+        if self.storage_type == constant.STORAGE_TYPE_CLOUDINARY:
+
+            # download the image, convert it to a NumPy array, and then read....
+
+            with urllib.request.urlopen(file_name) as url:
+                image = np.asarray(bytearray(url.read()), dtype='uint8'
+                                   )
+
+            # it into OpenCV format
+
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
             # return the image
+
             return image
         elif self.storage_type == constant.STORAGE_TYPE_S3:
             pass
